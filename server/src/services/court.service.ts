84f8ba68227a4a30c4a3fd03c88d6fc199d024b7
chayannepaulino <chayannepaulino@gmail.com>
2026-04-28 17:05:34 -0400

    fix table name type

--- server/src/services/court.service.ts
+++ server/src/services/court.service.ts
@@ -1,6 +1,6 @@
 import { pool } from "../config/db";
 
 export const fetchAllCourts = async () => {
-  const [rows] = await pool.query('SELECT * FROM Coach');
+  const [rows] = await pool.query('SELECT * FROM Court');
   return rows;
 };
